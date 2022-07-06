79395b792e7dc1e1ab2f7baf5ef96b5c1912331c
Nick Jones <nickjones9605@gmail.com>
2022-07-05 22:21:46 -0600

    Add test for displaying artist name

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,6 @@ class SongsController < ApplicationController
     end
 
     def show
-        
+        @song = Song.find(params[:id])
     end
 end
--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -15,5 +15,13 @@ RSpec.describe "the songs show page" do
         expect(page).to_not have_content(song2.title)
     end
 
-    it 'displays the name of the artist for the song'
+    it 'displays the name of the artist for the song' do
+        artist = Artist.create(name: 'Carly Rae Jepsen')
+        song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 265808)
+        song2 = artist.songs.create!(title: "Call Me Maybe", length: 230, play_count: 999999)
+
+        visit "/songs/#{song.id}"
+        # save_and_open_page
+        expect(page).to have_content(artist.name)
+    end
 end
